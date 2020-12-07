7c40dbc960eda1740feebfab999b1037a87b6f4e
Guilherme Azevedo <guiazetri@uninove.edu.br>
2020-12-07 15:05:16 -0300

    Branch Electron

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toobar-title>Most used Words</v-toobar-title>
+    </v-app-bar>
+
+    <v-content>
+      <Home />
+    </v-content>
+
+  </v-app>
+</template>
+
+<script >
+import Home from  './components/Home'
+export default {
+  name: "App",
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
